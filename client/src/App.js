14dda4a09a1c6931fb1d0d1bf0032fd44a574708
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-01-10 23:24:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,3 +8,23 @@ function c3(){sendToArduino('E');}
 function c4(){sendToArduino('R');}
 function c5(){sendToArduino('T');}
 function c6(){sendToArduino('Y');}
+
+function T(){
+  /*
+  radarReady(1)
+  var i = 0
+  var tt = setInterval(()=>{
+    i+=10
+    setRadar("GS-1-"+i+"-12-12")
+    if(i==180){clearInterval(tt)}
+  }, 300) 
+  setTimeout(()=>{
+    var i = 180
+  var tt = setInterval(()=>{
+    i-=10
+    setRadar("GS-2-"+i+"-12-12")
+    if(i==0){clearInterval(tt)}
+  }, 300) 
+  }, 6000)
+  */
+}
