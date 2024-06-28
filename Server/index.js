55b42417c6acae0ecd69eded1fec3663cad9ad2d
Anushka Isuru <129645615+Anushkai1@users.noreply.github.com>
2024-06-28 23:42:05 +0530

    Config PORT

--- Server/index.js
+++ Server/index.js
@@ -2,3 +2,12 @@ const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 const parser = new ReadlineParser({ delimeter : '\r\n'});
 var fs = require('fs')
+
+const config = {
+  path: 'COM5',
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  autoOpen: false,
+};
